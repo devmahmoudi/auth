acb1c6c1309cc1b8890ba8f2c80800f7b99b3756
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2026-07-02 19:07:24 +0330

    feat(auth): add sign out functionality to home page
    - Add signOut button with loading spinner state
    - Navigate to /auth/login after successful sign out

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,27 @@
+import { Button, Spinner } from "@devmahmoudi/ui";
+import { useAuth } from "../contexts/AuthContext";
+import { useNavigate } from "react-router";
+import { useState } from "react";
+
 export default function Home() {
-    return (
-        <h1>Welcome</h1>
-    )
+  const { signOut } = useAuth();
+  const navigate = useNavigate();
+
+  const [signingOut, setSigningOut] = useState<boolean>(false);
+
+  const handleSignOut = () => {
+    setSigningOut(true);
+    signOut()
+      .then(() => navigate("/auth/login"))
+      .finally(() => setSigningOut(false));
+  };
+
+  return (
+    <div className="flex justify-center flex-col text-center p-10 gap-3">
+      <h1>Welcome</h1>
+      <Button onClick={handleSignOut}>
+        {signingOut ? <Spinner /> : "Logout"}
+      </Button>
+    </div>
+  );
 }
